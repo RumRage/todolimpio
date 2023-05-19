3775f6c865d17f3ade5718c4a1f67347778780cc
Rumrage <claravillarruel4@gmail.com>
2023-05-19 19:27:34 -0300

    Combo index ok

--- src/components/combos/ComboIndex.jsx
+++ src/components/combos/ComboIndex.jsx
@@ -1,82 +1,135 @@
 import { useEffect, useContext } from "react"
 import { Link } from "react-router-dom";
 import ComboContext from "../../Context/ComboContext";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { useTheme } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import Header from "../../components/Header";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+
 
 export const ComboIndex = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
     const { combos, getCombos, deleteCombo } = useContext(ComboContext);
     useEffect(() => {
     getCombos();
     }, [])
+
+    const columns = [
+        { field: "id", headerName: "Id", flex: 0.5 },
+        { field: "name", headerName: "Nombre", flex: 1 },
+        {
+          field: "service_id",
+          headerName: "Servicio",
+          flex: 1,
+          valueGetter: (params) => {
+            const combo = params.row;
+            return combo.services.map((service) => `${service.name} ($${service.price})`).join(", ");
+          },
+        },
+        { field: "price", headerName: "Precio", flex: 1 },
+        { field: "discount", headerName: "Descuento", flex: 1 },
+        { field: "total_price", headerName: "Precio total", flex: 1 },
+        {
+          field: "actions",
+          headerName: "Opciones",
+          flex: 1,
+          renderCell: (params) => (
+            <>
+              <Button
+              component={Link}
+              to={`/combos/${params.row.id}/edit`}
+              variant="contained"
+              startIcon={<ModeEditOutlineOutlinedIcon />}
+              >
+            
+              Editar
+            </Button>
+              <Button 
+                variant="contained"
+                onClick={() => deleteCombo(params.row.id)}
+                startIcon={<DeleteIcon />}>
+                Eliminar
+              </Button>
+            </>
+          ),
+        },
+    ];
     
     return (
-        <div className="mt-12">
-           <div className="flex justify-end m-2 p-2">
-                <Link to="/combos/create" className="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">Nuevo combo</Link>
-            </div>
-            <div className="relative overflow-x-auto">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
-                                Id
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Nombre
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Servicio
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Precio
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Descuento
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Total
-                            </th>
-                            <th scope="col" className="px-6 py-3">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {combos.map((combo) => {
-                        return (
-                            <tr key={combo.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <td className="px-6 py-4">{combo.id}</td>
-                                <td className="px-6 py-4">{combo.name}</td>
-                                <td>
-                                    <table>
-                                      <thead>
-                                        <tr>
-                                          <th>Nombre</th>
-                                          <th>Precio</th>
-                                        </tr>
-                                      </thead>
-                                      <tbody>
-                                        {combo.services.map((service) => (
-                                          <tr key={service.id}>
-                                            <td>{service.name}</td>
-                                            <td>{service.price}</td>
-                                          </tr>
-                                        ))}
-                                      </tbody>
-                                    </table>
-                                  </td>
-                                <td className="px-6 py-4">{combo.price}</td>
-                                <td className="px-6 py-4">{combo.discount}</td>
-                                <td className="px-6 py-4">{combo.total_price}</td>
-                                <td className="px-6 py-4 space-x-2">
-                                 <Link to={`/combos/${combo.id}/edit`} className="px-4 py-2 bg-green-500 hover:bg-green-700 text-white rounded-md">Editar</Link>
-                                 <button onClick={() => deleteCombo(combo.id)} className="px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded-md">Borrar</button>
-                                </td>
-                               
-                            </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </div>
+        <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Header
+                title="COMBOS"
+                subtitle="Listado de combos"
+            />
+            <Box>
+                <Button
+                    component={Link}
+                    to="/combos/create"
+                    sx={{
+                      backgroundColor: colors.blueAccent[700],
+                      color: colors.grey[100],
+                      fontSize: "14px",
+                      fontWeight: "bold",
+                      padding: "10px 20px",
+                      textDecoration: "none",
+                    }}
+                  >
+                    <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+                      Nuevo combo
+                </Button>
+            </Box>
+        </Box>
+        <Box
+            m="40px 0 0 0"
+            height="75vh"
+            sx={{
+              "& .MuiDataGrid-root": {
+                border: "none",
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom: "none",
+              },
+              "& .name-column--cell": {
+                color: colors.greenAccent[300],
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: colors.blueAccent[700],
+                borderBottom: "none",
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                backgroundColor: colors.primary[400],
+              },
+              "& .MuiDataGrid-footerContainer": {
+                borderTop: "none",
+                backgroundColor: colors.blueAccent[700],
+              },
+              "& .MuiCheckbox-root": {
+                color: `${colors.greenAccent[200]} !important`,
+              },
+              "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                color: `${colors.grey[100]} !important`,
+              },
+            }}
+            >
+            <DataGrid
+                rows={combos}
+                columns={columns}
+                components={{
+                  Toolbar: GridToolbar,
+                }}
+                pageSize={5}
+                disableSelectionOnClick
+            />
+            </Box>
+        </Box>
+        
         )
     }
 
