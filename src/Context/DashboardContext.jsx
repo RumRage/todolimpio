828e69a929a8efecf088487ccda107dd44e2d508
RumRage <86855486+RumRage@users.noreply.github.com>
2023-10-07 18:21:53 -0300

    Update DashboardContext.jsx

--- src/Context/DashboardContext.jsx
+++ src/Context/DashboardContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1/";
+axios.defaults.baseURL = "https://todolimpio-api-53a8e9cc657e.herokuapp.com/api/v1/";
 
 const DashboardContext = createContext();
 
