8cd6e26d9f76d649125c1b29cbbe986eb274f49e
RumRage <86855486+RumRage@users.noreply.github.com>
2023-10-08 04:20:09 -0300

    Update DashboardContext.jsx

--- src/Context/DashboardContext.jsx
+++ src/Context/DashboardContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+axios.defaults.baseURL = "https://egxmzwrpk0ntvwkm:zzd908u4ae743bnv@eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ovp73xk5ickx2wo1";
 
 const DashboardContext = createContext();
 
