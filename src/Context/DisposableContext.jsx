a29c440dc0d2560ae66f04fd015d709b7cf6e95c
RumRage <86855486+RumRage@users.noreply.github.com>
2023-10-08 03:40:10 -0300

    Update DisposableContext.jsx

--- src/Context/DisposableContext.jsx
+++ src/Context/DisposableContext.jsx
@@ -3,7 +3,9 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 
-axios.defaults.baseURL = "https://todolimpio-api-53a8e9cc657e.herokuapp.com/api/v1/";
+axios.defaults.baseURL = "mysql://egxmzwrpk0ntvwkm:zzd908u4ae743bnv@eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ovp73xk5ickx2wo1
+
+";
 
 const DisposableContext = createContext();
 
